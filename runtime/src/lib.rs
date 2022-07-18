71692f58e8c2b02e591869e060319e0e9c492b16
arthur-cw <arthur.moreira@cloudwalk.io>
2022-07-18 01:52:12 -0300

    rename node :-)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -107,8 +107,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-frontier-template"),
-	impl_name: create_runtime_str!("node-frontier-template"),
+	spec_name: create_runtime_str!("cloudwalk-network"),
+	impl_name: create_runtime_str!("cloudwalk-network"),
 	authoring_version: 1,
 	spec_version: 2,
 	impl_version: 2,
